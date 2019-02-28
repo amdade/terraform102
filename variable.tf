d47bcca71dc4e2610480560a83ad1a35a10795a0
root <root@ip-172-31-19-183.eu-west-1.compute.internal>
2019-02-28 02:25:58 +0000

    terraform for vpc

---
+++ variable.tf
@@ -0,0 +1,25 @@
+variable "instance_type" {
+	default = "t2.micro"
+	
+
+}
+
+variable "ami" {
+	default = "ami-0fad7378adf284ce0"
+}
+
+variable "key_name" {
+	default = "test-key"
+}
+
+variable "count" {
+	default = "3"
+}
+
+variable "Enviroment" {
+	default = "dev"
+}
+
+variable "Created_by" {
+	default = "ahmed"
+}
